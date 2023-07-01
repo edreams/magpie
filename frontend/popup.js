66162ef53998ba32a440c021c89b21d8c9e36c8d
Ph1n-Pham <pham_p1@denison.edu>
2023-07-01 11:18:39 -0400

    new button

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('save-link').addEventListener('click', saveLink);
   document.getElementById('summarize-link').addEventListener('click', summarizeAndSaveLink);
   document.getElementById('get-summaries').addEventListener('click', getSummaries);
+  document.getElementById('send-text').addEventListener('click', sendSelectedText);
 });
 
 function saveLink(e) {
@@ -9,12 +10,12 @@ function saveLink(e) {
 
   var status = document.getElementById('status');
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     var activeTab = tabs[0];
     var user_id = "123";  // Fixed user ID
 
     var req = new XMLHttpRequest();
-    var baseUrl = "http://localhost:5000/save-link";
+    var baseUrl = "https://soychile.org/save-link";
     var body = JSON.stringify({
       'url': activeTab.url,
       'user_id': user_id
@@ -46,12 +47,12 @@ function summarizeAndSaveLink(e) {
 
   var status = document.getElementById('status');
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     var activeTab = tabs[0];
     var user_id = "123";  // Fixed user ID
 
     var req = new XMLHttpRequest();
-    var baseUrl = "http://localhost:5000/summarize-and-save";
+    var baseUrl = "https://soychile.org/summarize-and-save";
     var body = JSON.stringify({
       'url': activeTab.url,
       'user_id': user_id
@@ -78,7 +79,7 @@ function getSummaries(e) {
   var user_id = "123";  // Fixed user ID
 
   var req = new XMLHttpRequest();
-  var baseUrl = "http://localhost:5000/get-summaries";
+  var baseUrl = "https://soychile.org/get-summaries";
   var body = JSON.stringify({
     'user_id': user_id
   });
@@ -94,15 +95,62 @@ function getSummaries(e) {
 
         summariesContainer.textContent = "";
 
-        response.summaries.forEach(function(summary) {
-          var summaryElement = document.createElement('p');
-          summaryElement.textContent = summary.link + ": " + summary.summary;
-          summariesContainer.appendChild(summaryElement);
-        });
+        if (response.summaries.length === 0) {
+          var noSummariesElement = document.createElement('p');
+          noSummariesElement.textContent = "No summaries available.";
+          summariesContainer.appendChild(noSummariesElement);
+        } else {
+          response.summaries.forEach(function(summary) {
+            var summaryElement = document.createElement('p');
+            summaryElement.textContent = summary.link + ": " + summary.summary;
+            summariesContainer.appendChild(summaryElement);
+          });
+        }
       } else {
-        status.textContent = "Error retrieving summaries. Please, try again.";
+        status.textContent = "Error retrieving summaries. Please try again.";
       }
     }
   }
 }
 
+
+function sendSelectedText() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'getSelectedText' }, function(response) {
+      if (response && response.selectedText) {
+        sendSelectedTextToBackend(response.selectedText);
+      }
+    });
+  });
+}
+
+
+function sendSelectedTextToBackend(selectedText) {
+  const data = {
+    text: selectedText
+  };
+
+  fetch('https://soychile.org/play-selected', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.blob(); // Ahora esperamos un objeto Blob desde el backend
+      } else {
+        throw new Error('Error sending text to the backend');
+      }
+    })
+    .then(function(audioBlob) {
+      // Reproducir el audio en la extensión de Chrome
+      const audioUrl = URL.createObjectURL(audioBlob);
+      const audio = new Audio(audioUrl);
+      audio.play();
+    })
+    .catch(function(error) {
+      console.error('Error in the request to the backend:', error);
+    });
+}
